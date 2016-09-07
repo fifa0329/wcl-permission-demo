ddd3730f1cbd8040e0b5451624e8b70747518f81
lingxiang.wang <lingxiang.wang@alibaba-inc.com>
2016-09-07 11:14:22 +0800

    我自己根据自己理解 改了一下

--- app/src/main/java/me/chunyu/spike/wcl_permission_demo/PermissionsActivity.java
+++ app/src/main/java/me/chunyu/spike/wcl_permission_demo/PermissionsActivity.java
@@ -1,5 +1,6 @@
 package me.chunyu.spike.wcl_permission_demo;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -12,6 +13,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 /**
  * 权限获取页面
@@ -24,48 +26,58 @@ public class PermissionsActivity extends AppCompatActivity {
     public static final int PERMISSIONS_DENIED = 1; // 权限拒绝
 
     private static final int PERMISSION_REQUEST_CODE = 0; // 系统权限管理页面的参数
-    private static final String EXTRA_PERMISSIONS =
+    private static final String EXTRA_PERMISSION =
             "me.chunyu.clwang.permission.extra_permission"; // 权限参数
     private static final String PACKAGE_URL_SCHEME = "package:"; // 方案
 
     private PermissionsChecker mChecker; // 权限检测器
-    private boolean isRequireCheck; // 是否需要系统权限检测, 防止和系统提示框重叠
 
     // 启动当前权限页面的公开接口
-    public static void startActivityForResult(Activity activity, int requestCode, String... permissions) {
+    public static void startActivityForResult(Activity activity, int requestCode, String permission) {
         Intent intent = new Intent(activity, PermissionsActivity.class);
-        intent.putExtra(EXTRA_PERMISSIONS, permissions);
+        intent.putExtra(EXTRA_PERMISSION, permission);
         ActivityCompat.startActivityForResult(activity, intent, requestCode, null);
     }
 
-    @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getIntent() == null || !getIntent().hasExtra(EXTRA_PERMISSIONS)) {
+        if (getIntent() == null || !getIntent().hasExtra(EXTRA_PERMISSION)) {
             throw new RuntimeException("PermissionsActivity需要使用静态startActivityForResult方法启动!");
         }
         setContentView(R.layout.activity_permissions);
-
         mChecker = new PermissionsChecker(this);
-        isRequireCheck = true;
     }
 
-    @Override protected void onResume() {
+    @Override
+    protected void onResume() {
+        Log.d("wlx", "onResume");
         super.onResume();
-        if (isRequireCheck) {
-            String[] permissions = getPermissions();
-            if (mChecker.lacksPermissions(permissions)) {
-                requestPermissions(permissions); // 请求权限
-            } else {
-                allPermissionsGranted(); // 全部权限都已获取
-            }
+
+
+        String permission = getPermission();
+        if (mChecker.lacksPermissions(permission)) {
+            //(1)第一次 fresh install false
+            //(2)用户第一次deny了 true  （相当于给用户一次反悔的机会），同时在这次之后加上 checkbox  不在勾选
+            //(3)勾选 不再提示
+            //09-06 22:33:57.918 19524-19524/me.chunyu.spike.wcl_permission_demo D/wlx: shouldShowRequestPermissionRationale=false
+//            boolean b = ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+//            Log.d("wlx", "shouldShowRequestPermissionRationale=" + b);
+//            if (b) {
+//                Log.d("wlx", "shouldShowRequestPermissionRationale");
+//                showPermissionRetryDialog();
+//            } else {
+//                Log.d("wlx", "requestPermissions");
+            requestPermissions(permission); // 请求权限
+//            }
         } else {
-            isRequireCheck = true;
+            allPermissionsGranted(); // 全部权限都已获取
         }
     }
 
     // 返回传递的权限参数
-    private String[] getPermissions() {
-        return getIntent().getStringArrayExtra(EXTRA_PERMISSIONS);
+    private String getPermission() {
+        return getIntent().getStringExtra(EXTRA_PERMISSION);
     }
 
     // 请求权限兼容低版本
@@ -90,13 +102,37 @@ public class PermissionsActivity extends AppCompatActivity {
      */
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == PERMISSION_REQUEST_CODE && hasAllPermissionsGranted(grantResults)) {
-            isRequireCheck = true;
-            allPermissionsGranted();
-        } else {
-            isRequireCheck = false;
-            showMissingPermissionDialog();
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            // for each permission check if the user grantet/denied them
+            // you may want to group the rationale in a single dialog,
+            // this is just an example
+            for (int i = 0, len = permissions.length; i < len; i++) {
+                String permission = permissions[i];
+                if (grantResults[i] == PackageManager.PERMISSION_DENIED) {
+                    boolean showRationale = ActivityCompat.shouldShowRequestPermissionRationale(this, permission);
+                    if (!showRationale) {
+                        // user denied flagging NEVER ASK AGAIN
+                        // you can either enable some fall back,
+                        // disable features of your app
+                        // or open another dialog explaining
+                        // again the permission and directing to
+                        // the app setting
+                        showPermissionSettingsDialog();
+                    } else if (Manifest.permission.RECORD_AUDIO.equals(permission)) {
+                        // user denied WITHOUT never ask again
+                        // this is a good place to explain the user
+                        // why you need the permission and ask if he want
+                        // to accept it (the rationale)
+                        showPermissionRetryDialog();
+                    } else {
+                            /* possibly check more permissions...*/
+                    }
+                    return;
+                }
+            }
         }
+        allPermissionsGranted();
     }
 
     // 含有全部的权限
@@ -110,27 +146,57 @@ public class PermissionsActivity extends AppCompatActivity {
     }
 
     // 显示缺失权限提示
-    private void showMissingPermissionDialog() {
+    private void showPermissionRetryDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(PermissionsActivity.this);
         builder.setTitle(R.string.help);
-        builder.setMessage(R.string.string_help_text);
+        builder.setMessage(R.string.string_retry_text);
 
         // 拒绝, 退出应用
         builder.setNegativeButton(R.string.quit, new DialogInterface.OnClickListener() {
-            @Override public void onClick(DialogInterface dialog, int which) {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                setResult(PERMISSIONS_DENIED);
+                finish();
+            }
+        });
+
+        builder.setNeutralButton(R.string.sure, new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                requestPermissions(getPermission());
+            }
+        });
+
+        builder.setCancelable(false);
+        builder.show();
+    }
+
+
+    // 显示缺失权限提示
+    private void showPermissionSettingsDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(PermissionsActivity.this);
+        builder.setTitle(R.string.help);
+        builder.setMessage(R.string.string_setting_text);
+
+        // 拒绝, 退出应用
+        builder.setNegativeButton(R.string.quit, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
                 setResult(PERMISSIONS_DENIED);
                 finish();
             }
         });
 
         builder.setPositiveButton(R.string.settings, new DialogInterface.OnClickListener() {
-            @Override public void onClick(DialogInterface dialog, int which) {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
                 startAppSettings();
             }
         });
 
-        builder.setCancelable(false);
 
+        builder.setCancelable(false);
         builder.show();
     }
 
